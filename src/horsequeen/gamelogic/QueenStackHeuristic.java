0bb435f76bec7aa11606d48404236a1aaffd94bb
josue <josue@Josue_Portatil>
2013-05-30 13:51:47 +0100

    Añadida QueenStackHeuristic

---
+++ src/horsequeen/gamelogic/QueenStackHeuristic.java
@@ -0,0 +1,46 @@
+package horsequeen.gamelogic;
+
+import horsequeen.ia.Heuristic;
+import static horsequeen.gamelogic.HorseQueenStatus.WHITE;
+
+/**
+ * Heuristica que valora la cantidad de fichas apilasdas en la reina
+ * @author josue
+ */
+public class QueenStackHeuristic implements Heuristic{
+
+    @Override
+    public double h(HorseQueenStatus status, int player) {
+         if (player==WHITE){
+            if (status.getBlackQueen()==null 
+                    || status.getPosibleMovementsFor(status.getBlackQueen().getPosition())==null
+                    || status.getBlackQueen().getStack()==1){
+                return Double.POSITIVE_INFINITY;
+            }
+            else if (status.getWhiteQueen()==null 
+                    || status.getPosibleMovementsFor(status.getWhiteQueen().getPosition())==null
+                    || status.getWhiteQueen().getStack()==1){
+                return Double.NEGATIVE_INFINITY;
+            }
+        }
+        else{
+            if (status.getBlackQueen()==null 
+                    || status.getPosibleMovementsFor(status.getBlackQueen().getPosition())==null
+                    || status.getBlackQueen().getStack()==1){
+                return Double.NEGATIVE_INFINITY;
+            }
+            else if (status.getWhiteQueen()==null 
+                    || status.getPosibleMovementsFor(status.getWhiteQueen().getPosition())==null
+                    || status.getWhiteQueen().getStack()==1){
+                return Double.POSITIVE_INFINITY;
+            }
+        }
+        if (player==HorseQueenStatus.WHITE) 
+            return status.getWhiteQueen().getStack();
+        else 
+            return status.getBlackQueen().getStack();
+       
+    
+    }
+    
+}
