c131226a0881bae883fc3cfbc5a3c65412751f06
josue <josue@Josue_Portatil>
2013-05-30 13:49:10 +0100

    A falta de algunos retoques

--- src/horsequeen/Main.java
+++ src/horsequeen/Main.java
@@ -2,17 +2,26 @@ package horsequeen;
 
 import horsequeen.gamelogic.HorseQueenGame;
 import horsequeen.gamelogic.HorseQueenStatus;
+import horsequeen.gamelogic.Movement;
+import horsequeen.gamelogic.PositioningHeuristic;
+import horsequeen.gamelogic.PossibleMovementsHeuristic;
+import horsequeen.ia.AdversarialSearch;
+import horsequeen.ia.AlphaBetaSearch;
+import horsequeen.ia.Heuristic;
 import horsequeen.iu.OPanel;
 import horsequeen.iu.Tablero;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 /**
  *
@@ -22,6 +31,8 @@ public class Main extends JFrame {
 
     private Tablero tablero;
     private OPanel opciones;
+    HorseQueenGame horseQueenGame;
+    AdversarialSearch search;
 
     public Main() {
 
@@ -29,7 +40,12 @@ public class Main extends JFrame {
         this.setSize(new Dimension(801, 601));
         this.setVisible(true);
         this.setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
-
+        horseQueenGame = new HorseQueenGame();
+        horseQueenGame.setWhitePlayerHeuristic(new PositioningHeuristic());
+        horseQueenGame.setBlackPlayerHeuristic(new PositioningHeuristic());
+        search = new AlphaBetaSearch(horseQueenGame, 2);
+        
+       
         
         JButton resetButton = new JButton("Reset");
         resetButton.addActionListener(new ActionListener() {
@@ -44,17 +60,54 @@ public class Main extends JFrame {
 
             @Override
             public void actionPerformed(ActionEvent e) {
+                opciones.getDesicionLabel().setVisible(true);
+                Movement move = 
+                        (Movement)
+                        search.makeDecision(horseQueenGame.getActualStatus());
+                horseQueenGame.move(move);
+                opciones.getDesicionLabel().setVisible(false);
+                tablero.update();
                 
             }
         });
+        
+        JComboBox<Heuristic> whiteHeuristicComboBox = new JComboBox<>();
+        whiteHeuristicComboBox.addItem(new PositioningHeuristic());
+        whiteHeuristicComboBox.addItem(new PossibleMovementsHeuristic());
+        whiteHeuristicComboBox.addItemListener(new ItemListener() {
+
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                horseQueenGame.setWhitePlayerHeuristic((Heuristic) e.getItem());
+            }
+        });
+        
+        JComboBox<Heuristic> blackHeuristicComboBox = new JComboBox<>();
+        blackHeuristicComboBox.addItem(new PositioningHeuristic());
+        blackHeuristicComboBox.addItem(new PossibleMovementsHeuristic());
+        blackHeuristicComboBox.addItemListener(new ItemListener() {
+
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                horseQueenGame.setBlackPlayerHeuristic((Heuristic) e.getItem());
+            }
+        });
+        
+        
+        
 
         opciones = new OPanel(600, 80);
         opciones.setResetButton(resetButton);
+        opciones.setMakeDecisionButton(makeDecisionButton);
+        opciones.setWhiteHeuristicComboBox(whiteHeuristicComboBox);
+        opciones.setBlackHeuristicComboBox(blackHeuristicComboBox);
+        opciones.setDesicionLabel();
         opciones.setVisible(true);
         opciones.setBackground(Color.lightGray);
         this.add(opciones);
+        
         tablero = new Tablero(HorseQueenStatus.ROWS, HorseQueenStatus.COLS,
-                600, 600, new HorseQueenGame());
+                600, 600, horseQueenGame);
         tablero.setVisible(true);
         this.add(tablero);
         pack();
@@ -73,8 +126,12 @@ public class Main extends JFrame {
 
     private void reset() {
         remove(tablero);
+        horseQueenGame = new HorseQueenGame();
+        horseQueenGame.setWhitePlayerHeuristic(new PositioningHeuristic());
+        horseQueenGame.setBlackPlayerHeuristic(new PositioningHeuristic());
+        search = new AlphaBetaSearch(horseQueenGame, 2);
         tablero = new Tablero(HorseQueenStatus.ROWS, HorseQueenStatus.COLS,
-                600, 600, new HorseQueenGame());
+                600, 600, horseQueenGame);
         add(tablero);
         repaint();
         pack();
